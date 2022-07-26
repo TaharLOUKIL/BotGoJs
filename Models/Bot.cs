875929e37bb78c8a0f787c52d4ec273128fce777
PC LOUKIL <PC LOUKIL@LOUKILCODECARE>
2022-07-26 21:28:44 +0200

    Création du modèle Bot

---
+++ Models/Bot.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotGoJs.Models
+{
+    public class Bot  
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+
+        public string name { get; set; }
+    }
+}
