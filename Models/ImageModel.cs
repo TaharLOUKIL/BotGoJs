90f7cbd9b96008e65163f5991d2d73b183495e17
PC LOUKIL <PC LOUKIL@LOUKILCODECARE>
2022-07-26 21:46:33 +0200

    Création du modèles ImageModel

--- Models/ImageModel.cs
+++ Models/ImageModel.cs
@@ -1,6 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,20 +6,19 @@ using System.Threading.Tasks;
 namespace BotGoJs.Models
 {
     /// <summary>
-    /// Modèle qui servira à la persistance des textes
+    /// Modèle qui servira à la persistance des images
     /// </summary>
-    public class TextModel 
+    public class Image 
     {
-        [BsonRepresentation(BsonType.ObjectId)]
         public string _id { get; set; }
 
-        public string message { get; set; }
+        public string Titre { get; set; }
+        public string url { get; set; }
 
         public DateTime createdAt { get; set; }
 
         public DateTime modifiedAt { get; set; }
 
         public string type { get; set; }
-
     }
 }
