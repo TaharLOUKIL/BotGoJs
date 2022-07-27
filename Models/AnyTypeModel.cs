a16413b04b05d98b1cdbcdc45c8d3cc1e1d44a5a
PC LOUKIL <loukil.tahar@gmail.com>
2022-07-27 21:46:52 +0200

    Création du modèle AnyTypeModel

---
+++ Models/AnyTypeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotGoJs.Models
+{
+    public class AnyTypeModel
+    {
+        public List<AudioModel> audio { get; set; }
+
+        public List<FileModel> file { get; set; }
+
+
+        public List<ImageModel> image { get; set; }
+
+
+        public List<LocationModel> location { get; set; }
+
+
+        public List<TextModel> text { get; set; }
+
+        public List<VideoModel> video { get; set; }
+
+    }
+}
