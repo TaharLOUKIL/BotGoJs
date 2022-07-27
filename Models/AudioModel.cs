264dfb60fda2fe131bf30c9ffb2ab7d80ee84e03
PC LOUKIL <PC LOUKIL@LOUKILCODECARE>
2022-07-27 21:30:31 +0200

    Création du modèle Audio

---
+++ Models/AudioModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotGoJs.Models
+{
+    public class Audio 
+    {
+        public string _id { get; set; }
+
+        public string Titre { get; set; }
+        public string url { get; set; }
+
+        public DateTime createdAt { get; set; }
+
+        public DateTime modifiedAt { get; set; }
+
+        public string type { get; set; }
+    }
+}
