eafb5238ab43d6ea39a5a946dc006e9c19d8b8c9
PC LOUKIL <loukil.tahar@gmail.com>
2022-07-27 21:43:12 +0200

    Création du modèle Localisation

---
+++ Models/LocationModel.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotGoJs.Models
+{
+    public class LocationModel  
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+
+        public DateTime createdAt { get; set; }
+
+        public DateTime modifiedAt { get; set; }
+
+        public string Longitude { get; set; }
+
+        public string Lattitude { get; set; }
+
+        public string Adresse { get; set; }
+
+        public string Titre { get; set; }
+        public string type { get; set; }
+    }
+}
