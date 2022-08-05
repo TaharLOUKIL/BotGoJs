7732accf0fb5517dabc6c7d8ebec8eb24b167ef4
PC LOUKIL <loukil.tahar@gmail.com>
2022-08-05 22:38:27 +0200

    Nettoyage du code

--- Controllers/AudioController.cs
+++ Controllers/AudioController.cs
@@ -1,11 +1,9 @@
 ﻿using BotGoJs.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -16,121 +14,113 @@ namespace BotGoJs.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class AudioController : ControllerBase
-    { 
-     public readonly IConfiguration _configuration;
-
-    public AudioController(IConfiguration configuration)
     {
-        _configuration = configuration;
-    }
+        public readonly IConfiguration _configuration;
 
+        public AudioController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
-
-
-    [HttpGet]
-    public JsonResult get()
-    {
+        [HttpGet]
+        public JsonResult get()
+        {
             AudioModel audio = new AudioModel();
-            
-        return audio.LoadAll();
+
+            return audio.LoadAll();
         }
 
-    [HttpPost, DisableRequestSizeLimit]
-    public async Task<JsonResult> upload()
-    {
-        try
+        [HttpPost, DisableRequestSizeLimit]
+        public async Task<JsonResult> upload()
         {
+            try
+            {
                 AudioModel audio = new AudioModel();
-            var formCollection = await Request.ReadFormAsync();
+                var formCollection = await Request.ReadFormAsync();
 
-            if (formCollection["url"].ToString().Length == 0)
-            {
-                var file = formCollection.Files.First();
-                var folderName = "Resources/audio";
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                if (file.Length > 0)
+                if (formCollection["url"].ToString().Length == 0)
                 {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    var file = formCollection.Files.First();
+                    var folderName = "Resources/audio";
+                    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                    if (file.Length > 0)
                     {
-                        file.CopyTo(stream);
+                        var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        var fullPath = Path.Combine(pathToSave, fileName);
+                        var dbPath = Path.Combine(folderName, fileName);
+                        using (var stream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+                        audio.Url = "http://localhost:12195/Resources/audio/" + fileName;
                     }
-                    audio.Url = "http://localhost:12195/Resources/audio/" + fileName;
                 }
+                else
+                {
+                    audio.Url = formCollection["url"];
+                }
+
+                audio.CreatedAt = DateTime.Now;
+                audio.ModifiedAt = DateTime.Now;
+                audio.Titre = formCollection["Titre"];
+                audio.Type = "Audio";
+                MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
+                audio.Id = ObjectId.GenerateNewId().ToString();
+                dbclient.GetDatabase("Gojs").GetCollection<AudioModel>("Audio").InsertOne(audio);
+                return get();
             }
-            else
+            catch (Exception ex)
             {
-                audio.Url = formCollection["url"];
+                return new JsonResult(StatusCode(500, $"Internal server error: {ex}"));
             }
-
-            audio.CreatedAt = DateTime.Now;
-            audio.ModifiedAt = DateTime.Now;
-            audio.Titre = formCollection["Titre"];
-            audio.Type = "Audio";
-            MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
-            audio.Id = ObjectId.GenerateNewId().ToString();
-            dbclient.GetDatabase("Gojs").GetCollection<AudioModel>("Audio").InsertOne(audio);
-            return get();
-        }
-        catch (Exception ex)
-        {
-            return new JsonResult(StatusCode(500, $"Internal server error: {ex}"));
         }
 
-
-    }
-
-    [HttpPut, DisableRequestSizeLimit]
-    public async Task<JsonResult> update()
-    {
-        try
+        [HttpPut, DisableRequestSizeLimit]
+        public async Task<JsonResult> update()
         {
+            try
+            {
                 AudioModel audio = new AudioModel();
-            var formCollection = await Request.ReadFormAsync();
+                var formCollection = await Request.ReadFormAsync();
 
-            if (formCollection["url"].ToString().Length == 0)
-            {
-                var file = formCollection.Files.First();
-                var folderName = "Resources/audio";
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                if (file.Length > 0)
+                if (formCollection["url"].ToString().Length == 0)
                 {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    var file = formCollection.Files.First();
+                    var folderName = "Resources/audio";
+                    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                    if (file.Length > 0)
                     {
-                        file.CopyTo(stream);
+                        var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        var fullPath = Path.Combine(pathToSave, fileName);
+                        var dbPath = Path.Combine(folderName, fileName);
+                        using (var stream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+                        audio.Url = "http://localhost:12195/Resources/audio/" + fileName;
                     }
-                    audio.Url = "http://localhost:12195/Resources/audio/" + fileName;
                 }
-            }
-            else
-            {
-                audio.Url = formCollection["url"];
-            }
+                else
+                {
+                    audio.Url = formCollection["url"];
+                }
 
-            MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
+                MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
 
-
-            audio.CreatedAt = DateTime.Parse(formCollection["createdAt"]);
-            audio.ModifiedAt = DateTime.Now;
-            audio.Titre = formCollection["Titre"];
-            audio.Id = formCollection["ID"];
+                audio.CreatedAt = DateTime.Parse(formCollection["createdAt"]);
+                audio.ModifiedAt = DateTime.Now;
+                audio.Titre = formCollection["Titre"];
+                audio.Id = formCollection["ID"];
                 audio.Type = "Audio";
                 var filter = Builders<AudioModel>.Filter.Eq("_id", audio.Id);
-            dbclient.GetDatabase("Gojs").GetCollection<AudioModel>("Audio").ReplaceOne(filter, audio);
-            return get();
+                dbclient.GetDatabase("Gojs").GetCollection<AudioModel>("Audio").ReplaceOne(filter, audio);
+                return get();
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(StatusCode(500, $"Internal server error: {ex}"));
+            }
         }
-        catch (Exception ex)
-        {
-            return new JsonResult(StatusCode(500, $"Internal server error: {ex}"));
-        }
-
-
-    }
 
         [HttpDelete("{id}")]
         public JsonResult Delete(string id)
@@ -139,6 +129,5 @@ namespace BotGoJs.Controllers
             audio.Delete(id);
             return get();
         }
-
     }
 }
