730e9148ac45ce194cb7ef5e831c8645c09e89ec
PC LOUKIL <loukil.tahar@gmail.com>
2022-08-21 00:12:03 +0200

    Déplacement des CRUD dans le modèle Fichier

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,9 +1,11 @@
 ﻿using BotGoJs.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -14,124 +16,50 @@ namespace BotGoJs.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class FileController : ControllerBase
+    { 
+     public readonly IConfiguration _configuration;
+
+    public FileController(IConfiguration configuration)
     {
-        public readonly IConfiguration _configuration;
-
-        public FileController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        [HttpDelete("{id}")]
-        public JsonResult Delete(string id)
-        {
-            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
-
-            var filter = Builders<FileModel>.Filter.Eq("_id", id);
-
-            dbClient.GetDatabase("Gojs").GetCollection<FileModel>("Fichier").DeleteOne(filter);
-
-            return get();
-        }
-
-        [HttpGet]
-        public JsonResult get()
-        {
-            MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
-            var dblist = dbclient.GetDatabase("Gojs").GetCollection<FileModel>("Fichier").AsQueryable();
-            return new JsonResult(dblist);
-        }
-
-        [HttpPost, DisableRequestSizeLimit]
-        public async Task<JsonResult> upload()
-        {
-            try
-            {
-                FileModel fichier = new FileModel();
-                var formCollection = await Request.ReadFormAsync();
-
-                if (formCollection["url"].ToString().Length == 0)
-                {
-                    var file = formCollection.Files.First();
-                    var folderName = "Resources/file";
-                    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                    if (file.Length > 0)
-                    {
-                        var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                        var fullPath = Path.Combine(pathToSave, fileName);
-                        var dbPath = Path.Combine(folderName, fileName);
-                        using (var stream = new FileStream(fullPath, FileMode.Create))
-                        {
-                            file.CopyTo(stream);
-                        }
-                        fichier.url = "http://localhost:12195/Resources/file/" + fileName;
-                    }
-                }
-                else
-                {
-                    fichier.url = formCollection["url"];
-                }
-
-                fichier.createdAt = DateTime.Now;
-                fichier.modifiedAt = DateTime.Now;
-                fichier.Titre = formCollection["Titre"];
-                fichier.type = "File";
-                MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
-                fichier._id = ObjectId.GenerateNewId().ToString();
-                dbclient.GetDatabase("Gojs").GetCollection<FileModel>("Fichier").InsertOne(fichier);
-                return get();
-            }
-            catch (Exception ex)
-            {
-                return new JsonResult(StatusCode(500, $"Internal server error: {ex}"));
-            }
-        }
-
-        [HttpPut, DisableRequestSizeLimit]
-        public async Task<JsonResult> update()
-        {
-            try
-            {
-                FileModel fichier = new FileModel();
-                var formCollection = await Request.ReadFormAsync();
-
-                if (formCollection["url"].ToString().Length == 0)
-                {
-                    var file = formCollection.Files.First();
-                    var folderName = "Resources/file";
-                    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                    if (file.Length > 0)
-                    {
-                        var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                        var fullPath = Path.Combine(pathToSave, fileName);
-                        var dbPath = Path.Combine(folderName, fileName);
-                        using (var stream = new FileStream(fullPath, FileMode.Create))
-                        {
-                            file.CopyTo(stream);
-                        }
-                        fichier.url = "http://localhost:12195/Resources/file" + fileName;
-                    }
-                }
-                else
-                {
-                    fichier.url = formCollection["url"];
-                }
-
-                MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
-
-                fichier.createdAt = DateTime.Parse(formCollection["createdAt"]);
-                fichier.modifiedAt = DateTime.Now;
-                fichier.Titre = formCollection["Titre"];
-                fichier._id = formCollection["ID"];
-                fichier.type = "File";
-                var filter = Builders<FileModel>.Filter.Eq("_id", fichier._id);
-                dbclient.GetDatabase("Gojs").GetCollection<FileModel>("Fichier").ReplaceOne(filter, fichier);
-                return get();
-            }
-            catch (Exception ex)
-            {
-                return new JsonResult(StatusCode(500, $"Internal server error: {ex}"));
-            }
-        }
+        _configuration = configuration;
     }
+
+
+    [HttpDelete("{id}")]
+    public JsonResult Delete(string id)
+    {
+            FileModel file = new FileModel (_configuration);
+            file.Delete(id);
+            return get();
+    }
+
+    [HttpGet]
+    public JsonResult get()
+    {
+            FileModel file = new FileModel(_configuration);
+            return file.LoadAll();
+        }
+
+    [HttpPost, DisableRequestSizeLimit]
+    public async Task<JsonResult> upload()
+    {
+            var formCollection = await Request.ReadFormAsync();
+            FileModel file = new FileModel(_configuration);
+            file.SaveAsync(formCollection);
+            return file.LoadAll();
+        }
+
+    [HttpPut, DisableRequestSizeLimit]
+    public async Task<JsonResult> update()
+    {
+            var formCollection = await Request.ReadFormAsync();
+            FileModel file = new FileModel(_configuration);
+            file.Update(formCollection);
+            return file.LoadAll();
+     }
+
+}
+
+
+
 }
--- Models/FileModel.cs
+++ Models/FileModel.cs
@@ -1,18 +1,193 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace BotGoJs.Models
 {
     public class FileModel
     {
+        #region Properties
+        [BsonRepresentation(BsonType.ObjectId)]
         public string _id { get; set; }
+        private string _titre;
+        private string _url;
+        private DateTime _createdAt;
+        private DateTime _modifiedAt;
+        private string _type;
+        private IConfiguration _configuration;
 
-        public string Titre { get; set; }
-        public string url { get; set; }
+        //public string Id
+        //{
+        //    get { return this._id; }
+        //    set { this._id = value; }
+        //}
 
-        public DateTime createdAt { get; set; }
+        public string Titre
+        {
+            get { return this._titre; }
+            set { this._titre = value; }
+        }
 
-        public DateTime modifiedAt { get; set; }
+        public string Url
+        {
+            get { return this._url; }
+            set { this._url = value; }
+        }
 
-        public string type { get; set; }
+        public DateTime CreatedAt
+        {
+            get { return this._createdAt; }
+            set { this._createdAt = value; }
+        }
+
+        public DateTime ModifiedAt
+        {
+            get { return this._modifiedAt; }
+            set { this._modifiedAt = value; }
+        }
+
+        public string Type
+        {
+            get { return this._type; }
+            set { this._type = value; }
+        }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        public FileModel()
+        {
+        }
+        public FileModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public JsonResult LoadAll()
+        {
+            MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
+            var dblist = dbclient.GetDatabase(_configuration["Variable:Databasename"]).GetCollection<FileModel>("Fichier").AsQueryable();
+            return new JsonResult(dblist);
+        }
+        public Boolean SaveAsync(IFormCollection formCollection)
+        {
+            try
+            {
+                FileModel fichier = new FileModel();
+
+                if (formCollection["url"].ToString().Length == 0)
+                {
+                    var file = formCollection.Files.First();
+                    var folderName = "Resources/file";
+                    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                    if (file.Length > 0)
+                    {
+                        var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        var fullPath = Path.Combine(pathToSave, fileName);
+                        var dbPath = Path.Combine(folderName, fileName);
+                        using (var stream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+                        fichier.Url = _configuration["Variable:FilesRoute"] + "file/" + fileName;
+                    }
+                }
+                else
+                {
+                    fichier.Url = formCollection["url"];
+                }
+
+                fichier.CreatedAt = DateTime.Now;
+                fichier.ModifiedAt = DateTime.Now;
+                fichier.Titre = formCollection["Titre"];
+                fichier.Type = "File";
+                MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
+                fichier._id = ObjectId.GenerateNewId().ToString();
+                dbclient.GetDatabase(_configuration["Variable:Databasename"]).GetCollection<FileModel>("Fichier").InsertOne(fichier);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+
+        }
+        public Boolean Update(IFormCollection formCollection)
+        {
+            try
+            {
+                FileModel fichier = new FileModel();
+                if (formCollection["url"].ToString().Length == 0)
+                {
+                    var file = formCollection.Files.First();
+                    var folderName = "Resources/file";
+                    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                    if (file.Length > 0)
+                    {
+                        var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        var fullPath = Path.Combine(pathToSave, fileName);
+                        var dbPath = Path.Combine(folderName, fileName);
+                        using (var stream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+                        fichier.Url = _configuration["Variable:FilesRoute"]+"file/" + fileName;
+                    }
+                }
+                else
+                {
+                    fichier.Url = formCollection["url"];
+                }
+
+                MongoClient dbclient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
+
+
+                fichier.CreatedAt = DateTime.Parse(formCollection["createdAt"]);
+                fichier.ModifiedAt = DateTime.Now;
+                fichier.Titre = formCollection["Titre"];
+                fichier._id = formCollection["ID"];
+                fichier.Type = "File";
+                var filter = Builders<FileModel>.Filter.Eq("_id", fichier._id);
+                dbclient.GetDatabase(_configuration["Variable:Databasename"]).GetCollection<FileModel>("Fichier").ReplaceOne(filter, fichier);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+        public Boolean Delete(string id)
+        {
+            try
+            {
+                MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("gojsConnection"));
+
+                var filter = Builders<FileModel>.Filter.Eq("_id", id);
+
+
+                dbClient.GetDatabase(_configuration["Variable:Databasename"]).GetCollection<FileModel>("Fichier").DeleteOne(filter);
+                return true;
+            }
+            catch (Exception)
+            {
+                // Ajout de log
+                return false;
+            }
+        }
+
+        #endregion Methods
     }
 }
